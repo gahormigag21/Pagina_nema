de80308f525e95270edf40559574f8dc0fc75336
gahormigag21 <gahormigag@gmail.com>
2025-04-29 12:12:17 -0500

    Agregar Hero, features y Radix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Features from "@/components/Layouts/Features";
 import Hero from "@/components/Layouts/Hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Features />
     </div>
   );
 }
